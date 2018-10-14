39225df6dd2ac75040d9f78c850565bc6345c08f
Peng Wu <42914141+pw444@users.noreply.github.com>
2018-10-14 16:29:52 -0400

    Udpate blink.py
    add comments for instructions

--- Lab3/blink.py
+++ Lab3/blink.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
+# set up GPIO mode
 GPIO.setmode(GPIO.BCM)
+# set GPIO 13 for output port
 GPIO.setup(13,GPIO.OUT)
-
-p=GPIO.PWM(13,0.5)
+# init the blink frequency
+freq = 0.5
+# set the PWM frequency
+p=GPIO.PWM(13,freq)
+# set duty cycle
 p.start(50)
+# waiting for keyboard to stop the program
 raw_input('Press return to stop:')
 p.stop()
+# clean up the GPIO 
 GPIO.cleanup()
